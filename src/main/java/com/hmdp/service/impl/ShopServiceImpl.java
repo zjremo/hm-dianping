f4227d7f146934b595fc05da1f7b531b527cdb02
zjremo <3106316195@qq.com>
2026-03-07 23:58:30 +0800

    2026-03-07 商户查询缓存功能初步实现(缓存和数据库双写，redis充当中间层缓存结果)

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -16,5 +25,43 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result queryById(Long id) {
+        // 1. redis中查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+        // 2. 查看是否存在
+        if (StrUtil.isNotBlank(shopJson)){
+            // 3. 存在，直接返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+
+        // 4. 不存在，根据ID查询数据库
+        Shop shop = getById(id);
+        // 5. 不存在，返回错误
+        if (shop == null){
+            return Result.fail("店铺不存在");
+        }
+        // 6. 存在，写入redis
+        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        return Result.ok(shop);
+    }
+
+    @Transactional
+    @Override
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if (id == null){
+            return Result.fail("店铺id不能为空");
+        }
+        // 1. 更新数据库
+        updateById(shop);
+        // 2. 删除缓存
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        stringRedisTemplate.delete(key);
+        return Result.ok();
+    }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,14 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -16,5 +25,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result querySort() {
+        // 1. 先从redis缓存中进行查看
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(RedisConstants.SHOP_TYPE_KEY);
+        if (StrUtil.isNotBlank(shopTypeJson)) {
+            // 2. 如果存在返回分类信息
+            return Result.ok(JSONUtil.toList(shopTypeJson, ShopType.class));
+        }
+
+        // 3. 从数据库里面查
+        List<ShopType> shopTypes = query().orderByAsc("sort").list();
+        if (shopTypes == null){
+            // 4. 不存在，此时返回错误信息
+            return Result.fail("没有分类数据");
+        }
+
+        // 5. 数据库中记录存在，则存入redis，然后返回分类信息
+        String json = JSONUtil.toJsonStr(shopTypes);
+        stringRedisTemplate.opsForValue().set(RedisConstants.SHOP_TYPE_KEY, json, RedisConstants.SHOP_TYPE_TTL, TimeUnit.MINUTES);
+        return Result.ok(shopTypes);
+    }
 }
