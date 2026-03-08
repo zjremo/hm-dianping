2f32735705e106c93aa2607a4a6172493fa419aa
zjremo <3106316195@qq.com>
2026-03-08 21:50:54 +0800

    2026-03-08 缓存空对象解决缓存穿透

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -30,23 +30,30 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
         // 1. redis中查询商铺缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
         // 2. 查看是否存在
         if (StrUtil.isNotBlank(shopJson)){
             // 3. 存在，直接返回
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
-
+        // ToDo 缓存穿透此时需要判断查到的是否是空值 此时不为null一定是空字符串""
+        if (shopJson != null){
+            return Result.fail("店铺信息不存在!");
+        }
         // 4. 不存在，根据ID查询数据库
         Shop shop = getById(id);
         // 5. 不存在，返回错误
         if (shop == null){
-            return Result.fail("店铺不存在");
+            // ToDo 抵御缓存穿透，将空值写入redis 通过缓存空对象来抵抗缓存穿透(一定要设置过期时间)
+            stringRedisTemplate.opsForValue().set(key, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return Result.fail("店铺不存在!");
         }
         // 6. 存在，写入redis
-        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        // 7. 返回结果
         return Result.ok(shop);
     }
 
